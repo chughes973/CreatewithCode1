8f6def80e73bc21268bd61f6554a95903057f0cd
chughes973 <146023719+chughes973@users.noreply.github.com>
2023-11-12 18:27:18 +0000

    6.2 Add a speedometer display

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    //Private Variables
-    //[SerializeField] float speed = 25.0f;
+    //Serialize Variables
+    [SerializeField] float speed = 25.0f;
     [SerializeField] private float horsePower = 0;
     [SerializeField] float turnSpeed = 25.0f;
     private float horizontalInput;
@@ -17,6 +18,7 @@ public class PlayerController : MonoBehaviour
     public KeyCode switchKey;
     private Rigidbody playerRb;
     [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
 
     // Start is called before the first frame update
     private void Start()
@@ -34,6 +36,8 @@ public class PlayerController : MonoBehaviour
         playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         // Rotate the vehicle instead of sliding
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f); //for kmph change 2.237 to 3.6
+        speedometerText.SetText("Speed: " + speed + "mph");
         //Change Camera view by pressing F button
         if(Input.GetKeyDown(switchKey))
         {
