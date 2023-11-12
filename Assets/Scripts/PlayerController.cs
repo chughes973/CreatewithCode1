7d17689805f61aafdf073de48464ed1e41c26979
chughes973 <146023719+chughes973@users.noreply.github.com>
2023-11-12 17:11:32 +0000

    Lesson 6.1 2.Unity Event Functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     //Private Variables
-    private float speed = 25.0f;
-    private float turnSpeed = 25.0f;
+    [SerializeField] float speed = 25.0f;
+    [SerializeField] float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
     //Public Variables
@@ -15,13 +16,9 @@ public class PlayerController : MonoBehaviour
     public KeyCode switchKey;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //This is where we get player input 
         horizontalInput = Input.GetAxis("Horizontal");
